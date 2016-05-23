84a3a5d8892c6696511acfdfefdcd58da84dac60
Thomas Kerbrat <kerbrat@outlook.com>
2016-05-23 17:59:27 +0200

    Add IVisitor interface and two visitors.

---
+++ NS.CalviScript/Visitors/IVisitor.cs
@@ -0,0 +1,17 @@
+﻿namespace NS.CalviScript
+{
+    public interface IVisitor
+    {
+        void VisitBinaryExpression(BinaryExpression expression);
+
+        void VisitConstantExpression(ConstantExpression expression);
+
+        void VisitErrorExpression(ErrorExpression expression);
+    }
+
+    public static class IVisitorExtentions
+    {
+        public static void Visit(this IVisitor @this, IExpression expression)
+            => expression.Accept(@this);
+    }
+}
---
+++ NS.CalviScript/Visitors/LispyStringVisitor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NS.CalviScript
+{
+    public class LispyStringVisitor : IVisitor
+    {
+        public string Result { get; private set; }
+
+        public void VisitBinaryExpression(BinaryExpression expression)
+        {
+            expression.LeftExpression.Accept(this);
+            string left = Result;
+            expression.RightExpression.Accept(this);
+            string right = Result;
+
+            Result = string.Format("[{0} {1} {2}]",
+                TokenTypeToString(expression.OperatorType),
+                left,
+                right);
+        }
+
+        public void VisitConstantExpression(ConstantExpression expression)
+            => Result = expression.Value.ToString();
+
+        public void VisitErrorExpression(ErrorExpression expression)
+            => Result = string.Format("[Error {0}]", expression.Message);
+
+        // TODO: Remove this shit.
+        string TokenTypeToString(TokenType type)
+        {
+            if (type == TokenType.Plus) return "+";
+            else if (type == TokenType.Minus) return "-";
+            else if (type == TokenType.Mult) return "*";
+            else if (type == TokenType.Div) return "/";
+            else
+            {
+                Debug.Assert(type == TokenType.Modulo);
+                return "%";
+            }
+        }
+    }
+}
