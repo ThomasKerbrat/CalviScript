0014ce4f1331ec93bb24192a959905937e46b835
Thomas Kerbrat <kerbrat@outlook.com>
2016-05-24 10:44:44 +0200

    Merge IExpressionTests file into ParserTests file.

--- NS.CalviScript.Tests/ParserTests.cs
+++ NS.CalviScript.Tests/ParserTests.cs
@@ -18,5 +18,23 @@ namespace NS.CalviScript.Tests
 
             Assert.That(expression.ToLispyString(), Is.EqualTo(expected));
         }
+
+        [Test]
+        public void should_stringify()
+        {
+            IExpression expression = new BinaryExpression(
+                TokenType.Plus,
+                new BinaryExpression(
+                    TokenType.Plus,
+                    new ConstantExpression(2),
+                    new ConstantExpression(7)),
+                new BinaryExpression(
+                    TokenType.Mult,
+                    new ConstantExpression(5),
+                    new ConstantExpression(8)));
+
+
+            Assert.That(expression.ToInfixString(), Is.EqualTo("((2 + 7) + (5 * 8))"));
+        }
     }
 }
