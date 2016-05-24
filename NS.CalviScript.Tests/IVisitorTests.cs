4e041270b987d881f8f1b903890f67578a9836cb
Thomas Kerbrat <kerbrat@outlook.com>
2016-05-24 10:04:46 +0200

    Add a new EvaluationVisitor and the tests.

--- NS.CalviScript.Tests/IVisitorTests.cs
+++ NS.CalviScript.Tests/IVisitorTests.cs
@@ -30,5 +30,19 @@ namespace NS.CalviScript.Tests
 
             Assert.That(visitor.Result, Is.EqualTo("(2 + (7 / 12))"));
         }
+
+        [TestCase("1", 1)]
+        [TestCase("1 + 1", 2)]
+        [TestCase("(3 + 7) / (5 - 2)", 3)]
+        public void should_evaluate(string input, int expected)
+        {
+            Parser parser = new Parser(new Tokenizer(input));
+            IExpression expression = parser.ParseExpression();
+            EvaluationVisitor visitor = new EvaluationVisitor();
+
+            visitor.Visit(expression);
+
+            Assert.That(visitor.Result, Is.EqualTo(expected));
+        }
     }
 }
---
+++ NS.CalviScript/Visitors/EvaluationVisitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NS.CalviScript
+{
+    public class EvaluationVisitor : IVisitor
+    {
+        public EvaluationVisitor() { }
+
+        public int Result { get; private set; }
+
+        public void Visit(ErrorExpression expression)
+        {
+            throw new InvalidOperationException(expression.Message);
+        }
+
+        public void Visit(ConstantExpression expression)
+            => Result = expression.Value;
+
+        public void Visit(BinaryExpression expression)
+        {
+            expression.LeftExpression.Accept(this);
+            int left = Result;
+            expression.RightExpression.Accept(this);
+            int right = Result;
+            Compute(left, right, expression.OperatorType);
+        }
+
+        private void Compute(int left, int right, TokenType operatorType)
+        {
+            switch (operatorType)
+            {
+                case TokenType.Plus:
+                    Result = left + right;
+                    break;
+                case TokenType.Minus:
+                    Result = left - right;
+                    break;
+                case TokenType.Mult:
+                    Result = left * right;
+                    break;
+                case TokenType.Div:
+                    Result = left / right;
+                    break;
+                case TokenType.Modulo:
+                    Result = left % right;
+                    break;
+                default:
+                    throw new ArgumentException("Not an operator.");
+            }
+        }
+    }
+}
